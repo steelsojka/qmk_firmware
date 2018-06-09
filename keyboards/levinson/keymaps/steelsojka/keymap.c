593aa6961a9b490e2483bbf086ee34917c5a1a76
U-DESKTOP-1ILLQN8\steel <steelsojka@gmail.com>
2018-06-08 22:33:31 -0500

    made right space always be right shift

--- keyboards/levinson/keymaps/steelsojka/keymap.c
+++ keyboards/levinson/keymaps/steelsojka/keymap.c
@@ -14,7 +14,6 @@ extern keymap_config_t keymap_config;
 #define _PAD 5
 #define _ADJUST 16
 
-#define RSFT_SP MT(MOD_RSFT, KC_SPC)
 #define ESC_CTRL MT(MOD_LCTL, KC_ESC)
 
 enum custom_keycodes {
@@ -39,14 +38,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | PAD  | Ctrl | Alt  | GUI  |Lower |Space |Rs/Sp | Raise| GUI  | Alt  | Ctrl | PAD  |
+ * | PAD  | Ctrl | Alt  | GUI  |Lower |Space | Shift| Raise| GUI  | Alt  | Ctrl | PAD  |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT( \
   KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   ESC_CTRL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT, \
-  PAD,      KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  RSFT_SP, RAISE,   KC_RGUI, KC_RALT, KC_RCTL, PAD \
+  PAD,      KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_RSFT, RAISE,   KC_RGUI, KC_RALT, KC_RCTL, PAD \
 ),
 
 /* Lower
@@ -87,19 +86,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
- * |      |      |  F7  |  F8  |  F9  | F10  |      |   7  |   8  |   9  |   -  | Bksp |
+ * |      |  F1  |  F2  |  F3  |  F4  |      |      |   7  |   8  |   9  |   -  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |  F4  |  F5  |  F6  | F11  |      |   4  |   5  |   6  |   +  |      | 
+ * |      |  F5  |  F6  |  F7  |  F8  |      |      |   4  |   5  |   6  |   +  |      | 
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |  F1  |  F2  |  F3  | F12  |      |   1  |   2  |   3  |   /  |Enter |
+ * |      |  F9  |  F10 |  F11 |  F12 |      |      |   1  |   2  |   3  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |   0  |   0  |   .  |   *  |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_PAD] = LAYOUT( \
-  _______, _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, KC_7, KC_8, KC_9, KC_MINUS,  KC_BSPC, \
-  _______, _______, KC_F4,   KC_F5,   KC_F6,   KC_F11,  _______, KC_4, KC_5, KC_6, KC_PLUS,   _______, \
-  _______, _______, KC_F1,   KC_F2,   KC_F3,   KC_F12,  _______, KC_1, KC_2, KC_3, KC_SLASH,  KC_ENTER, \
+  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______, _______, KC_7, KC_8, KC_9, KC_MINUS,  KC_BSPC, \
+  _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______, _______, KC_4, KC_5, KC_6, KC_PLUS,   _______, \
+  _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, KC_1, KC_2, KC_3, KC_SLASH,  KC_ENTER, \
   _______, _______, _______, _______, _______, _______, _______, KC_0, KC_0, KC_DOT, KC_ASTR, _______ \
 ),
 
