c1be929c8855eef94f6d5fb3f1e36b813c87e1ca
U-DESKTOP-1ILLQN8\steel <steelsojka@gmail.com>
2018-06-11 01:25:42 -0500

    add backlight breathing

--- keyboards/levinson/keymaps/steelsojka/config.h
+++ keyboards/levinson/keymaps/steelsojka/config.h
@@ -40,5 +40,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
+#define BACKLIGHT_BREATHING
 
 #endif
--- keyboards/levinson/keymaps/steelsojka/keymap.c
+++ keyboards/levinson/keymaps/steelsojka/keymap.c
@@ -116,7 +116,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_ADJUST] =  LAYOUT( \
   _______, RESET  , RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUI, RGB_SAD, RGB_SAI, RGB_VAD, RGB_VAI, _______, _______, \
   _______, _______, _______, AU_ON,   AU_OFF , AG_NORM, AG_SWAP, QWERTY , _______, _______, _______, _______, \
-  _______, _______, BL_DEC , BL_INC , _______, BL_BRTG, _______, _______, _______, _______, _______, _______, \
+  _______, _______, BL_DEC , BL_INC , BL_TOGG, BL_BRTG, _______, _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
 )
 
