0b63adc15979b0f751fbd4494bfcb999726fd020
U-DESKTOP-1ILLQN8\steel <steelsojka@gmail.com>
2018-06-15 01:18:02 -0500

    add colemak back in

--- keyboards/levinson/keymaps/steelsojka/keymap.c
+++ keyboards/levinson/keymaps/steelsojka/keymap.c
@@ -9,6 +9,7 @@ extern keymap_config_t keymap_config;
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 #define _QWERTY 0
+#define _COLEMAK 1
 #define _LOWER 3
 #define _RAISE 4
 #define _PAD 5
@@ -18,6 +19,7 @@ extern keymap_config_t keymap_config;
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
+  COLEMAK,
   LOWER,
   RAISE,
   ADJUST,
@@ -41,11 +43,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | PAD  | Ctrl | Alt  | GUI  |Lower |Space | Shift| Raise| GUI  | Alt  | Ctrl | PAD  |
  * `-----------------------------------------------------------------------------------'
  */
-[_QWERTY] = LAYOUT( \
+[_QWERTY] = LAYOUT_ortho_4x12( \
   KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   ESC_CTRL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT, \
   PAD,      KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_RSFT, RAISE,   KC_RGUI, KC_RALT, KC_RCTL, PAD \
+),
+
+/* Colemak
+ * ,-----------------------------------------------------------------------------------.
+ * | Tab  |   Q  |   W  |   F  |   P  |   G  |   J  |   L  |   U  |   Y  |   ;  | Bksp |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | Esc  |   A  |   R  |   S  |   T  |   D  |   H  |   N  |   E  |   I  |   O  |  "   |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   K  |   M  |   ,  |   .  |   /  |Enter |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | PAD  | Ctrl | Alt  | GUI  |Lower |Space | Shift| Raise| GUI  | Alt  | Ctrl | PAD  |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_COLEMAK] = LAYOUT_ortho_4x12( \
+  KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC, \
+  KC_ESC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
+  PAD,     KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_RSFT, RAISE,   KC_RGUI, KC_RALT, KC_RCTL, PAD \
 ),
 
 /* Lower
@@ -59,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |      |      |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
-[_LOWER] = LAYOUT( \
+[_LOWER] = LAYOUT_ortho_4x12( \
   KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_MINUS, KC_EQUAL, KC_BSPC, \
   KC_DEL,  _______, _______, _______, KC_LPRN, KC_LCBR, KC_RCBR, KC_RPRN, KC_UNDS, KC_PLUS,  KC_BSLS,  KC_PIPE, \
   _______, _______, _______, _______, KC_LT,   KC_LBRC, KC_RBRC, KC_GT,   _______, _______,  _______,  _______, \
@@ -77,7 +97,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      | Bksp |      |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
-[_RAISE] = LAYOUT( \
+[_RAISE] = LAYOUT_ortho_4x12( \
   KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,     KC_0,    KC_BSPC, \
   KC_DEL,  _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_RBRC, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, \
@@ -95,7 +115,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |      |      |   0  |   0  |   .  |   *  |      |
  * `-----------------------------------------------------------------------------------'
  */
-[_PAD] = LAYOUT( \
+[_PAD] = LAYOUT_ortho_4x12( \
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______, _______, KC_7, KC_8, KC_9, KC_MINUS,  KC_BSPC, \
   _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______, _______, KC_4, KC_5, KC_6, KC_PLUS,   _______, \
   _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, KC_1, KC_2, KC_3, KC_SLASH,  KC_ENTER, \
@@ -113,10 +133,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
-[_ADJUST] =  LAYOUT( \
+[_ADJUST] =  LAYOUT_ortho_4x12( \
   _______, RESET  , RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUI, RGB_SAD, RGB_SAI, RGB_VAD, RGB_VAI, _______, _______, \
-  _______, _______, _______, AU_ON,   AU_OFF , AG_NORM, AG_SWAP, QWERTY , _______, _______, _______, _______, \
-  _______, _______, BL_DEC , BL_INC , BL_TOGG, BL_BRTG, _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, AU_ON,   AU_OFF , AG_NORM, AG_SWAP, _______, _______, _______, QWERTY , _______, \
+  _______, _______, BL_DEC , BL_INC , BL_TOGG, BL_BRTG, _______, _______, _______, _______, COLEMAK, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
 )
 
@@ -140,6 +160,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           PLAY_SONG(tone_qwerty);
         #endif
         persistent_default_layer_set(1UL<<_QWERTY);
+      }
+      return false;
+      break;
+    case COLEMAK:
+      if (record->event.pressed) {
+        persistent_default_layer_set(1UL<<_COLEMAK);
       }
       return false;
       break;
