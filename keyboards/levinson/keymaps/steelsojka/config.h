854cc7df03086b106cae7cfab538702870730542
U-DESKTOP-1ILLQN8\steel <steelsojka@gmail.com>
2018-06-28 17:26:07 -0500

    add some short cuts

--- keyboards/levinson/keymaps/steelsojka/config.h
+++ keyboards/levinson/keymaps/steelsojka/config.h
@@ -41,5 +41,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
 #define BACKLIGHT_BREATHING
+#define PREVENT_STUCK_MODIFIERS
 
 #endif
