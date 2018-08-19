d84200205e466b2d4e2ff16fceccb911575e769c
U-DESKTOP-1ILLQN8\steel <steelsojka@gmail.com>
2018-08-19 06:45:48 -0500

    remove tarmak layers

--- keyboards/levinson/keymaps/steelsojka/keymap.c
+++ keyboards/levinson/keymaps/steelsojka/keymap.c
@@ -9,10 +9,6 @@ extern keymap_config_t keymap_config;
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 #define _QWERTY 0
-#define _TARMAK_1 1
-#define _TARMAK_2 2
-#define _TARMAK_3 3
-#define _TARMAK_4 4
 #define _COLEMAK 5
 #define _LOWER 6
 #define _RAISE 7
@@ -24,10 +20,6 @@ extern keymap_config_t keymap_config;
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   COLEMAK,
-  TARMAK_1,
-  TARMAK_2,
-  TARMAK_3,
-  TARMAK_4,
   LOWER,
   RAISE,
   ADJUST,
@@ -55,78 +47,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   ESC_CTRL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT, \
-  PAD,      KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_RSFT, RAISE,   KC_RGUI, KC_RALT, KC_RCTL, PAD \
-),
-
-/* Tarmak 1
- * ,-----------------------------------------------------------------------------------.
- * | Tab  |   Q  |   W  |   J  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |EsCtrl|   A  |   S  |   D  |   F  |   G  |   H  |   N  |   E  |   L  |   ;  |  "   |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   K  |   M  |   ,  |   .  |   /  |Enter |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | PAD  | Ctrl | Alt  | GUI  |Lower |Space | Shift| Raise| GUI  | Alt  | Ctrl | PAD  |
- * `-----------------------------------------------------------------------------------'
- */
-[_TARMAK_1] = LAYOUT_ortho_4x12( \
-  KC_TAB,   KC_Q,    KC_W,    KC_J,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-  ESC_CTRL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_N,    KC_E,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT, \
-  PAD,      KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_RSFT, RAISE,   KC_RGUI, KC_RALT, KC_RCTL, PAD \
-),
-
-/* Tarmak 2
- * ,-----------------------------------------------------------------------------------.
- * | Tab  |   Q  |   W  |   F  |   R  |   G  |   Y  |   U  |   I  |   O  |   P  | Bksp |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |EsCtrl|   A  |   S  |   D  |   T  |   J  |   H  |   N  |   E  |   L  |   ;  |  "   |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   K  |   M  |   ,  |   .  |   /  |Enter |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | PAD  | Ctrl | Alt  | GUI  |Lower |Space | Shift| Raise| GUI  | Alt  | Ctrl | PAD  |
- * `-----------------------------------------------------------------------------------'
- */
-[_TARMAK_2] = LAYOUT_ortho_4x12( \
-  KC_TAB,   KC_Q,    KC_W,    KC_F,    KC_R,    KC_G,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-  ESC_CTRL, KC_A,    KC_S,    KC_D,    KC_T,    KC_J,    KC_H,    KC_N,    KC_E,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT, \
-  PAD,      KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_RSFT, RAISE,   KC_RGUI, KC_RALT, KC_RCTL, PAD \
-),
-
-/* Tarmak 3
- * ,-----------------------------------------------------------------------------------.
- * | Tab  |   Q  |   W  |   F  |   J  |   G  |   Y  |   U  |   I  |   O  |   P  | Bksp |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |EsCtrl|   A  |   R  |   S  |   T  |   D  |   H  |   N  |   E  |   L  |   ;  |  "   |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   K  |   M  |   ,  |   .  |   /  |Enter |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | PAD  | Ctrl | Alt  | GUI  |Lower |Space | Shift| Raise| GUI  | Alt  | Ctrl | PAD  |
- * `-----------------------------------------------------------------------------------'
- */
-[_TARMAK_3] = LAYOUT_ortho_4x12( \
-  KC_TAB,   KC_Q,    KC_W,    KC_F,    KC_J,    KC_G,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-  ESC_CTRL, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT, \
-  PAD,      KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_RSFT, RAISE,   KC_RGUI, KC_RALT, KC_RCTL, PAD \
-),
-
-/* Tarmak 4
- * ,-----------------------------------------------------------------------------------.
- * | Tab  |   Q  |   W  |   F  |   R  |   G  |   J  |   U  |   I  |   Y  |   ;  | Bksp |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |EsCtrl|   A  |   R  |   S  |   T  |   D  |   H  |   N  |   E  |   L  |   O  |  "   |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   K  |   M  |   ,  |   .  |   /  |Enter |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | PAD  | Ctrl | Alt  | GUI  |Lower |Space | Shift| Raise| GUI  | Alt  | Ctrl | PAD  |
- * `-----------------------------------------------------------------------------------'
- */
-[_TARMAK_4] = LAYOUT_ortho_4x12( \
-  KC_TAB,   KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_U,    KC_I,    KC_Y,    KC_SCLN,    KC_BSPC, \
-  ESC_CTRL, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_L,    KC_O, KC_QUOT, \
-  KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT, \
   PAD,      KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_RSFT, RAISE,   KC_RGUI, KC_RALT, KC_RCTL, PAD \
 ),
 
@@ -184,29 +104,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, KC_BSPC, _______, _______, KC_MNXT, KC_VOLD,  KC_VOLU, KC_MPLY \
 ),
 
-/* Lower
+/* Pad
  * ,-----------------------------------------------------------------------------------.
  * |      |  F1  |  F2  |  F3  |  F4  |      |      |   7  |   8  |   9  |   -  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |  F5  |  F6  |  F7  |  F8  |      |      |   4  |   5  |   6  |   +  |      |
+ * |      |  F5  |  F6  |  F7  |  F8  |      | Tab  |   4  |   5  |   6  |   +  |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |  F9  |  F10 |  F11 |  F12 |      |      |   1  |   2  |   3  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |   0  |   0  |   .  |   *  |      |
+ * |      |      |      |      |      |      | Space|   0  |   0  |   .  |   *  |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_PAD] = LAYOUT_ortho_4x12( \
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______, _______, KC_7, KC_8, KC_9, KC_MINUS,  KC_BSPC, \
-  _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______, _______, KC_4, KC_5, KC_6, KC_PLUS,   _______, \
+  _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______, KC_TAB , KC_4, KC_5, KC_6, KC_PLUS,   _______, \
   _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, KC_1, KC_2, KC_3, KC_SLASH,  KC_ENTER, \
-  _______, _______, _______, _______, _______, _______, _______, KC_0, KC_0, KC_DOT, KC_ASTR, _______ \
+  _______, _______, _______, _______, _______, _______, KC_SPC , KC_0, KC_0, KC_DOT, KC_ASTR, _______ \
 ),
 
 /* Adjust (Lower + Raise)
  * ,-----------------------------------------------------------------------------------.
  * |      | Reset|      |      |      |      |      |      |      |      |      |  Del |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |Aud on|Audoff|AGnorm|AGswap|Qwerty|      |      |      |      |
+ * |      |      |      |Aud on|Audoff|AGnorm|AGswap|Qwerty|Colmak|      |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -215,8 +135,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ADJUST] =  LAYOUT_ortho_4x12( \
   _______, RESET  , RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUI, RGB_SAD,  RGB_SAI,  RGB_VAD,  RGB_VAI,  _______, _______, \
-  _______, _______, _______, _______, _______, _______, QWERTY ,  _______,  _______,  _______,  _______, _______, \
-  _______, _______, BL_DEC , BL_INC , BL_TOGG, BL_BRTG, TARMAK_1, TARMAK_2, TARMAK_3, TARMAK_4, COLEMAK, _______, \
+  _______, _______, _______, _______, _______, _______, QWERTY ,  COLEMAK,  _______,  _______,  _______, _______, \
+  _______, _______, BL_DEC , BL_INC , BL_TOGG, BL_BRTG, _______,  _______,  _______,  _______,  _______, _______, \
   _______, _______, _______, _______, _______, _______, _______,  _______,  _______,  _______,  _______, _______ \
 )
 
@@ -246,30 +166,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case COLEMAK:
       if (record->event.pressed) {
         persistent_default_layer_set(1UL<<_COLEMAK);
-      }
-      return false;
-      break;
-    case TARMAK_1:
-      if (record->event.pressed) {
-        persistent_default_layer_set(1UL<<_TARMAK_1);
-      }
-      return false;
-      break;
-    case TARMAK_2:
-      if (record->event.pressed) {
-        persistent_default_layer_set(1UL<<_TARMAK_2);
-      }
-      return false;
-      break;
-    case TARMAK_3:
-      if (record->event.pressed) {
-        persistent_default_layer_set(1UL<<_TARMAK_3);
-      }
-      return false;
-      break;
-    case TARMAK_4:
-      if (record->event.pressed) {
-        persistent_default_layer_set(1UL<<_TARMAK_4);
       }
       return false;
       break;
